07bf777dfddbf5c70920541dcc7c7440065da183
Murali Padavala <mpadavala@apple.com>
2022-09-23 15:59:20 -0500

    cleanup
    cleanup

---
+++ src/main/java/com/labs/algorithms/Palindrome.java
@@ -0,0 +1,55 @@
+package com.labs.algorithms;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Palindrome {
+	
+	//"A man, a plan, a canal: Panama"
+	//"race a car"
+/*
+	public static void main(String args[]) {
+		
+		//String str1 = "A man, a plan, a canal: Panama";
+		//String str2 = "race a car";
+		
+		//System.out.println(str1 + " : "+ isPalindrome(str1));
+		//System.out.println(str1 + " : "+ isPalindrome(str2));
+		
+		String str3 = "abbwa";
+		System.out.println(str3 + " : "+ isPalindrome(str3));
+		
+	}
+	
+	Map<Char, Char> invalidCharactersMap = new HashMap<Char, Char>();
+	
+	invalidCharactersMap.add(' ', ' ');
+	
+	public static boolean isValidCharacter(Character character) {
+		
+	}
+	
+	public static boolean isPalindrome(String str){
+		
+		
+		int first=0;
+		int second=str.length()-1;
+		Character char1;
+		Character char2;
+		while(first <= second) {
+			if(first >= second) return true;
+			char1 = str.charAt(first);
+			char2 = str.charAt(second);
+			//TODO :: // check those are valid characters
+			if()
+			if(String.valueOf(char1).equalsIgnoreCase(String.valueOf(char2))){
+				first++;
+				second--;
+			}else
+				return false;
+		}
+	
+		return true;
+	}
+	*/
+}
